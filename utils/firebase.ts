8f2366868f6383ab8f2438db774593a955a3058e
amish-sherlekar-202 <amishsherlekar10@gmail.com>
2025-03-31 09:18:17 +0530

    modified:   utils/firebase.ts

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,26 +1,25 @@
 import { initializeApp } from 'firebase/app';
 
 // Optionally import the services that you want to use
-// import {...} from "firebase/auth";
-// import {...} from "firebase/database";
-// import {...} from "firebase/firestore";
-// import {...} from "firebase/functions";
-// import {...} from "firebase/storage";
+import {getAnalytics} from "firebase/analytics";
+import {getAuth} from "firebase/auth";
+import {getDatabase} from "firebase/database";
+import {getFirestore} from "firebase/firestore";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDRfxmzugRxD6VrZ7QCoYqiaaWrA8topdg",
+  authDomain: "voxa-rn.firebaseapp.com",
+  databaseURL: "https://voxa-rn-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "voxa-rn",
+  storageBucket: "voxa-rn.firebasestorage.app",
+  messagingSenderId: "326440922618",
+  appId: "1:326440922618:web:f766e17b05619772254843",
+  measurementId: "G-TJK7CLB02G"
 };
 
-const firebase = initializeApp(firebaseConfig);
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
-
-export default firebase;
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app)
+const firestore = getFirestore(app)
+const db = getDatabase(app)
+export default {app, analytics, firestore, db};
